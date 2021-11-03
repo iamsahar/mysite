983091371238f94dab2e200b3b7cc67e95ac1af4
Farahzad <shr.farahzad@gmail.com>
2021-11-03 22:40:10 +0330

    add models for contact and post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+from importlib.resources import contents
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # image =
+    # author = 
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tags = 
+    # category = 
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
