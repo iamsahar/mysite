7ac16c20ea6b54d3e2ac1e900d1fe03a8350c374
Farahzad <shr.farahzad@gmail.com>
2021-11-03 23:56:53 +0330

    added post in admin

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,5 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return "{} - {}".format(self.title, self.id)
