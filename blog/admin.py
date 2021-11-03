37e966e265a469bbd59c487d5aaa333cf8ea2945
Farahzad <shr.farahzad@gmail.com>
2021-11-04 00:29:28 +0330

    modified admin for post

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    list_display = ("title", "counted_views", "status", "published_date", "created_date")
+    list_filter = ("status",)
+    ordering = ["-created_date"]
+    search_fields = ["title", "content"]
+admin.site.register(Post, PostAdmin)
