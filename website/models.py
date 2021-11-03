144a6b178319a4cdec71dbca8f7c6b0454fd59bb
Farahzad <shr.farahzad@gmail.com>
2021-11-04 00:53:45 +0330

    add contact to admin and class meta for contact

--- website/models.py
+++ website/models.py
@@ -7,3 +7,8 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["created_date"]
+
+    def __str__(self):
+        return self.name
