f57d80460b2b4072921842f730fe1a86dedf15e0
Farahzad <shr.farahzad@gmail.com>
2021-11-05 19:07:30 +0330

    blog urls, views and templates are modified to add data in blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name='index'),
-    path('single', blog_single, name='single'),
+    path('<int:pid>', blog_single, name='single'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from blog.models import Post
+from django.utils import timezone
+import datetime
 
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = Post.objects.filter(published_date__lte=datetime.date.today())
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
 
-def blog_single(request):
-    return render(request, "blog/blog-single.html")
+def blog_single(request, pid):
+    post = Post.objects.get(id=pid)
+    post.counted_views= post.counted_views + 1
+    post.save()
+    context = {"post": post}
+    return render(request, "blog/blog-single.html", context)
