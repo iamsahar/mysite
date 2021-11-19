c96025caf5101e2f70a2ec96e85bb61c7fd2f6bc
Sahar Farahzad <49515562+iamsahar@users.noreply.github.com>
2021-11-19 05:16:00 +0330

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Category
+from blog.models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.utils import timezone
 
 def blog_view(request, **kwargs):
@@ -8,6 +9,16 @@ def blog_view(request, **kwargs):
         posts = posts.filter(category__name=kwargs["cat_name"])
     if kwargs.get("author_username"):
         posts = posts.filter(author__username=kwargs["author_username"])
+
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get("page")
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
