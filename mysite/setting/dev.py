3daf7d6eafac2f6f47ea40624e9227c6403b124d
Sahar Farahzad <49515562+iamsahar@users.noreply.github.com>
2022-01-20 04:00:50 +0330

    ready to deploy

---
+++ mysite/setting/dev.py
@@ -0,0 +1,33 @@
+from mysite.settings import *
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#dp#3ocj21)7vy980809oi0ae!heu^+7jmn3@&(4ao^9%d5)(v'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# sites framework
+SITE_ID = 2
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
