0ec96063fe3deca1113d10a0e1747752a7f639a4
Sahar Farahzad <49515562+iamsahar@users.noreply.github.com>
2022-01-20 04:04:52 +0330

    added 404 page

--- blog/views.py
+++ blog/views.py
@@ -62,3 +62,6 @@ def blog_search(request):
             posts = posts.filter(content__contains=s)
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
